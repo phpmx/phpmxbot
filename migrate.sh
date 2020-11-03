247f040a421b1fbfe0db801055155ee6db54f1d7
Erick Ruiz de Chavez <erick@erch.co>
2020-11-03 14:34:46 -0500

    Add root user

--- migrate.sh
+++ migrate.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run -ti --rm -v $PWD/db:/flyway/db -v $PWD/db/sql:/flyway/sql -v $PWD/db/conf:/flyway/conf flyway/flyway ${1:-migrate} -n
+docker run -ti --rm -v $PWD/db:/flyway/db -v $PWD/db/sql:/flyway/sql -v $PWD/db/conf:/flyway/conf --user root flyway/flyway ${1:-migrate} -n
