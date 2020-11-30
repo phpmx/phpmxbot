f0372ec3358a447faa600175bd1cba96bdaf8563
Erick Ruiz de Chavez <erick@erch.co>
2020-11-30 17:44:51 -0500

    Add tests

---
+++ tests/Unit/Services/GreeterTest.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpMx\Tests\Unit\Services;
+
+use PhpMx\Services\Greeter;
+use PHPUnit\Framework\TestCase;
+use SQLite3;
+use SQLite3Result;
+use SQLite3Stmt;
+
+final class GreeterTest extends TestCase
+{
+    public function provideGreeterMessages(): array
+    {
+        return [
+            'no messages' => ['foo', []],
+            'one message' => ['bar', ['one message']],
+            'multiple messages' => ['baz', ['one message', 'two messages']],
+        ];
+    }
+
+    /**
+     * @param string $channel
+     * @param array $messages
+     * @dataProvider provideGreeterMessages
+     */
+    public function testGetMessagesFor(string $channel, array $messages): void
+    {
+        $resultMock = $this->createMock(SQLite3Result::class);
+        $resultMock->expects($this->exactly(count($messages) + 1))
+            ->method('fetchArray')
+            ->with(SQLITE3_ASSOC)
+            ->willReturnOnConsecutiveCalls(...$messages);
+
+        $queryMock = $this->createMock(SQLite3Stmt::class);
+        $queryMock->expects($this->once())
+            ->method('bindValue')
+            ->with(':channel', $channel);
+        $queryMock->expects($this->once())
+            ->method('execute')
+            ->willReturn($resultMock);
+
+        $sqlMock = $this->createMock(SQLite3::class);
+        $sqlMock->expects($this->once())
+            ->method('prepare')
+            ->willReturn($queryMock);
+
+        $greeter = new Greeter($sqlMock);
+        $result = $greeter->getMessagesFor($channel);
+
+        $this->assertEquals($messages, $result);
+    }
+}
